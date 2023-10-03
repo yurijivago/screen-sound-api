543f7a955c626947a71ae715245fffc1cb43a64f
Yuri Jivago <yurijivagoads@gmail.com>
2023-10-03 08:35:10 -0300

    Desserializando os dados e exibindo, por enquanto de forma estática, os dados de uma música.

--- ScreenSoudAPI/ScreenSoudAPI/Program.cs
+++ ScreenSoudAPI/ScreenSoudAPI/Program.cs
@@ -1,9 +1,15 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using ScreenSoudAPI.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
 {
     try {
         string apiAddress = "https://guilhermeonrails.github.io/api-csharp-songs/songs.json";
         string resposta = await client.GetStringAsync(apiAddress);
-        Console.WriteLine(resposta);
+
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        Console.WriteLine($"Temos {musicas.Count} músicas nessa lista");
+        musicas[100].ExibirDetalhesDaMusica();
     }
     catch (Exception ex)
     {
