1c8aabcf2e691c0d39e7588043f7120bde938bf1
Yuri Jivago <yurijivagoads@gmail.com>
2023-10-03 08:22:05 -0300

    Criando a 1a requisiçao, expondo os dados no console e tratando uma Exception

---
+++ ScreenSoudAPI/ScreenSoudAPI/Program.cs
@@ -0,0 +1,15 @@
+﻿using (HttpClient client = new HttpClient())
+{
+    try {
+        string apiAddress = "https://guilhermeonrails.github.io/api-csharp-songs/songs.json";
+        string resposta = await client.GetStringAsync(apiAddress);
+        Console.WriteLine(resposta);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+    
+
+    
+}
