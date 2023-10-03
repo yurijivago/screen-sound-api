719053e2a89e7a65275c83cd784b94072af0dcf0
Yuri Jivago <yurijivagoads@gmail.com>
2023-10-03 08:29:09 -0300

    Criando modelo Musica

---
+++ ScreenSoudAPI/ScreenSoudAPI/Modelos/Musica.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace ScreenSoudAPI.Modelos;
+
+internal class Musica
+{
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    public void ExibirDetalhesDaMusica()
+    {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Música: {Nome}");
+        Console.WriteLine($"Duração: {Duracao / 1000}");
+        Console.WriteLine($"Gênero musical: {Genero}");
+    }
+
+}
